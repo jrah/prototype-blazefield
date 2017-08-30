075814578d5dc9b48e41873edd3f2fc8d7eebd78
jrah <jjames.home@gmail.com>
2017-08-30 23:43:45 +0100

    Airbnb linter compliant

--- src/javascript/carousel.js
+++ src/javascript/carousel.js
@@ -1,31 +1,32 @@
-carousel = (function(){
-  var box = document.querySelector('.carousel');
-  var next = box.querySelector('.next');
-  var prev = box.querySelector('.prev');
-  var items = box.querySelectorAll('.content li');
-  var counter = 0;
-  var amount = items.length;
-  var current = items[0];
+  const box = document.querySelector('.carousel');
+  const next = box.querySelector('.next');
+  const prev = box.querySelector('.prev');
+  const items = box.querySelectorAll('.content li');
+  const amount = items.length;
+  let counter = 0;
+  let current = items[0];
+
   box.classList.add('active');
+
   function navigate(direction) {
     current.classList.remove('current');
-    counter = counter + direction;
+    counter += direction;
     if (direction === -1 &&
-        counter < 0) {
+      counter < 0) {
       counter = amount - 1;
     }
     if (direction === 1 &&
-        !items[counter]) {
+      !items[counter]) {
       counter = 0;
     }
     current = items[counter];
     current.classList.add('current');
   }
-  next.addEventListener('click', function(ev) {
+
+  navigate(0);
+  next.addEventListener('click', () => {
     navigate(1);
   });
-  prev.addEventListener('click', function(ev) {
+  prev.addEventListener('click', () => {
     navigate(-1);
   });
-  navigate(0);
-})();
